b45cf832ed1059a6e6156bd7ae62a8012b7f5194
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-10 19:55:01 +0300

    add changes in Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -3,8 +3,11 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
+int min = num1;
 
 if (num1>max) max = num1;
+if (num1<min) min = num1;
 if (num2>max) max = num2;
+if (num2<min) min = num2;
 
-Console.WriteLine("max = " + max);
+Console.WriteLine("max = " + max + "; min = " + min);
