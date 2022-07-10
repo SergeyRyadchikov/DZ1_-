d0d5af9eac2972a198dcbc3bfdc009852853da1c
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-10 18:44:03 +0300

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+
+if (num1>max) max = num1;
+if (num2>max) max = num2;
+
+Console.WriteLine("max = " + max);
