92669fc8bf6fbac89c3efa741a5f9d8b5e19a839
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-10 19:38:34 +0300

    add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+if (N > 0)
+{
+    while (result <= N)
+    {
+        if (result % 2 == 0)
+        {
+            Console.Write(result);
+            result++;
+        }
+        else
+        {
+            result++;
+        }
+    }
+}
+
+else
+{
+    result = -1;
+    while (result >= N)
+    {
+       if (result % 2 == 0)
+        {
+            Console.Write(result);
+            result--;
+        }
+        else
+        {
+            result--;
+        }
+    }
+}
